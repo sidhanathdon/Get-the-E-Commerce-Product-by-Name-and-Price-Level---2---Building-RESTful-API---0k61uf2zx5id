037b8e8ce70103268a7635c4fb68f8496a9a1372
sidhanathdon <137877586+sidhanathdon@users.noreply.github.com>
2024-01-03 15:25:23 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -10,5 +10,30 @@ app.use(express.json());
 
 // GET endpoint for sending the products to client by id
 //// Endpoint - /api/v1/products/:id
-
+app.get("/api/v1/products/:id",(req,res)=>{
+  let {id}=req.params;
+  id *=1;
+  if(!isNaN(id)){
+    const product=products.find(product=> product.id===id);
+    if(!product){
+      res.status(404).json({
+        status:"failed",
+        message:"Product not found!",
+      });
+    }else{
+    res.status(200).json({
+      status:"success",
+      message:"Product fetched successfully",
+      data:{
+        product,
+      },
+    })
+  }
+  }else{
+    res.status(400).json({
+      status:"failed",
+      message:"Product not found",
+    })
+  }
+})
 module.exports = app;
